63f20f01eb72cba54cc0b261cd54c417d7515682
Rokas Gulbinas <rkgulbinas@gmail.com>
2020-04-23 00:32:50 +0300

    Update layout

---
+++ Web/Helpers/ActiveLinkHelper.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Web.Helpers
+{
+    public static class ActiveLinkHelper
+    {
+        public static string IsActiveLink(string actionName, ViewContext viewContext)
+        {
+            return viewContext.RouteData.Values["Action"].ToString() == actionName ? "link-active" : string.Empty;
+        }
+    }
+}
