b2acc5075821ea9993af72994d8c8e131b3753c9
Rokas Gulbinas <rkgulbinas@gmail.com>
2020-04-26 00:31:14 +0300

    Add required attr

--- Web/ViewModels/ExpenseViewModel.cs
+++ Web/ViewModels/ExpenseViewModel.cs
@@ -1,11 +1,15 @@
-﻿namespace Web.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.ViewModels
 {
     public class ExpenseViewModel
     {
         public int Id { get; set; }
 
+        [Required]
         public double? Amount { get; set; }
 
+        [Required]
         public string Purpose { get; set; }
 
         public string Note { get; set; }
