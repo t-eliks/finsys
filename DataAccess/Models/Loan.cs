b640a1187d85dd7e8041417da3311fd6534769b3
Lukas Bernotas <lukasbernotas@icloud.com>
2020-05-19 18:38:23 +0300

    Add Loan

---
+++ DataAccess/Models/Loan.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataAccess.Models
+{
+    public class Loan
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public float Sum { get; set; }
+
+        public float Interest { get; set; }
+
+        public DateTime Term { get; set; }
+
+        public string Provider { get; set; }
+
+        public float ReturnedSum { get; set; }
+
+        public string Type { get; set; }
+    }
+}
--- DataAccess/Repository.cs
+++ DataAccess/Repository.cs
@@ -5,13 +5,18 @@ namespace DataAccess
 {
     public class Repository : DbContext
     {
-        public Repository(DbContextOptions<Repository> options) : base(options) { }
+        public Repository(DbContextOptions<Repository> options) : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
 
         public virtual DbSet<User> Users { get; set; }
 
         public virtual DbSet<FinancialGuru> FinancialGurus { get; set; }
-
+        
         public virtual DbSet<Expense> Expenses { get; set; }
+        
+        public virtual DbSet<Loan> Loans { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -20,6 +25,7 @@ namespace DataAccess
             builder.Entity<User>().ToTable("User");
             builder.Entity<FinancialGuru>().ToTable("FinancialGuru");
             builder.Entity<Expense>().ToTable("Expense");
+            builder.Entity<Loan>().ToTable("Loan");
         }
     }
 }
