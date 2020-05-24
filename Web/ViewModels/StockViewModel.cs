dd4bf2275ef43e3e6df8438acf8d125db9f8af05
Lukas Žiūkas <lukasziukas18@gmail.com>
2020-05-24 13:22:50 +0300

    Little fix
    LIDL

--- Web/ViewModels/StockViewModel.cs
+++ Web/ViewModels/StockViewModel.cs
@@ -7,13 +7,10 @@ namespace Web.ViewModels
     {
         public int Id { get; set; }
         
-        [Required]
         public string Name { get; set; }
         
-        [Required]
         public string Company { get; set; }
         
-        [Required]
         public int? Amount { get; set; }
     }
 }
